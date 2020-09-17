c4ce3566c1a48390457b6af8f15482b522c60f55
Mikhail-off <nmmikhaylov@edu.hse.ru>
2020-09-18 01:03:50 +0300

    train refactoring

--- make_keras_dataset.py
+++ make_keras_dataset.py
@@ -1,35 +1,81 @@
 import pandas as pd
 import numpy as np
 import os
-from shutil import copyfile
+from PIL import Image
 from tqdm import tqdm
 import argparse
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument("--src_images", "-src", type=str,
+argparser.add_argument("--src_dataset", "-src", type=str,
                        help="Путь до папки с датасетом")
-argparser.add_argument("--dst_file", "-dst", type=str,
+argparser.add_argument("--dst_dataset", "-dst", type=str,
                        help="Путь до папки с датасетом для обучения")
+argparser.add_argument("--train_rate", "-tr", type=float, default=True,
+                       help="Процент изображений для обучения")
+argparser.add_argument("--crop", action='store_true',
+                       help="Нужно ли обрезать картинку")
+argparser.add_argument("--n_classes", type=int,
+                       help="Сколько классов в датаете")
+
+args = argparser.parse_args()
+
+RAW_DATASET_PATH = args.src_dataset
+TRAIN_DATASET_PATH = args.dst_dataset
+NEED_CROP = args.crop
+TRAIN_RATE = args.train_rate
+N_CLASSES = args.n_classes
+
+CLASS_COL = 'Class'
+CROP_COLS = ['TopLeftX', 'TopLeftY', 'BottomRightX', 'BottomRightY']
+MARKUP_EXT = '.csv'
+DST_IMAGE_EXT = '.png'
 
 
-RAW_DATA_PATH = 'C:\\GlassesDetetion\\raw_data\\'
-DATA_PATH = 'C:\\GlassesDetetion\\data\\'
-RAW_IMAGES_PATH = os.path.join(RAW_DATA_PATH, 'images')
-MARKUP_FILE_PATH = os.path.join(RAW_DATA_PATH, 'markup.csv')
+def is_image(file_path):
+    return os.path.splitext(file_path)[-1] in ['.jpg', '.png', '.JPG', '.JPEG']
 
-NAME_COL = 'name'
-CLASS_COL = 'class'
 
-def copy_image_to_folder(image_name, image_class):
-    image_class = str(image_class)
-    image_class_dir = os.path.join(DATA_PATH, image_class)
-    os.makedirs(image_class_dir, exist_ok=True)
-    copyfile(os.path.join(RAW_IMAGES_PATH, image_name), os.path.join(image_class_dir, image_name))
+def form_dataset(image_names, dst_dataset_folder):
+    for i in range(N_CLASSES):
+        os.makedirs(os.path.join(dst_dataset_folder, str(i)), exist_ok=True)
+
+    obj_ind = 0
+    for image_name in tqdm(image_names):
+        image_name, orig_image_ext = os.path.splitext(image_name)
+        markup_name = image_name + MARKUP_EXT
+        df = pd.read_csv(os.path.join(RAW_DATASET_PATH, 'Markup', markup_name))
+        for i in range(len(df)):
+            obj_class = df[CLASS_COL].values[i]
+            image = Image.open(os.path.join(RAW_DATASET_PATH, 'Image', image_name + orig_image_ext)).convert('RGB')
+            if NEED_CROP:
+                image = image.crop(box=df[CROP_COLS].values[i])
+
+            obj_ind += 1
+            dst_image_path = os.path.join(dst_dataset_folder, str(obj_class), '%06d' % obj_ind + DST_IMAGE_EXT)
+            image.save(dst_image_path)
+
 
 def main():
-    markup_df = pd.read_csv(MARKUP_FILE_PATH)
-    for image_name, image_class in tqdm(markup_df.values):
-        copy_image_to_folder(image_name, image_class)
+    image_names = np.array(list(filter(is_image, os.listdir(os.path.join(RAW_DATASET_PATH, 'Image')))))
+    np.random.shuffle(image_names)
+
+    train_left = 0
+    train_right = int(len(image_names) * TRAIN_RATE)
+    train_image_names = image_names[train_left:train_right]
+
+    test_left = train_right
+    test_right = test_left + int(len(image_names) * (1 - TRAIN_RATE) / 2)
+    test_image_names = image_names[test_left:test_right]
+
+    val_left = test_right
+    val_right = len(image_names)
+    val_image_names = image_names[val_left:val_right]
+
+    for name, cur_image_names in zip(['Train', 'Test', 'Validation'],
+                                     [train_image_names, test_image_names, val_image_names]):
+        dataset_path = os.path.join(TRAIN_DATASET_PATH, name)
+        form_dataset(cur_image_names, dataset_path)
+
 
 if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -7,19 +7,41 @@ import matplotlib.pyplot as plt
 import cv2
 
 import keras
-from keras.layers import Dense, Flatten, GlobalAveragePooling2D
+from keras.layers import Dense, Flatten, GlobalAveragePooling2D, Dropout
 from keras.models import Model, load_model
 from keras.applications.mobilenet_v2 import MobileNetV2
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
+import argparse
 
-BATCH_SIZE = 8
-IMAGE_SIZE = 224
-SEED = 24
-IMAGE_COUNT = 1000#47917
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--train_dataset", "-train", type=str,
+                       help="Путь до папки с датасетом")
+argparser.add_argument("--test_dataset", "-test", type=str,
+                       help="Путь до папки с датасетом для обучения")
+argparser.add_argument("--n_classes", type=int,
+                       help="Сколько классов в датаете")
+argparser.add_argument("--learning_rate", "-lr", type=float, default=1e-3,
+                       help="Learning rate во время обучения")
+argparser.add_argument("--continue_from_model", "-cfm", type=str, default=None,
+                       help="Загрузить модель для дообучения")
+argparser.add_argument("--batch_size", "-bs", type=int, default=8,
+                       help="Batch size для обучения")
+argparser.add_argument("--epochs", "-e", type=int, default=1,
+                       help="Кол-во эпох обучения")
 
-DATASET_PATH = 'C:\\GlassesDetetion\\data\\'
+args = argparser.parse_args()
+
+TRAIN_DATASET_PATH = args.train_dataset
+TEST_DATASET_PATH = args.test_dataset
+BATCH_SIZE = args.batch_size
+EPOCHS = args.epochs
+N_CLASSES = args.n_classes
+LR = args.learning_rate
+
 MODEL_NAME = 'model.hdf5'
+IMAGE_SIZE = 224
+SEED = 42
 
 
 def normalize_image(img_as_array):
@@ -29,33 +51,48 @@ def normalize_image(img_as_array):
 def train_generator(data_dir):
     return ImageDataGenerator(preprocessing_function=normalize_image).flow_from_directory(
         data_dir, batch_size=BATCH_SIZE,
-        class_mode='binary', target_size=(IMAGE_SIZE, IMAGE_SIZE),
+        class_mode='sparse', target_size=(IMAGE_SIZE, IMAGE_SIZE),
         color_mode="rgb", seed=SEED)
 
 
+def test_generator(data_dir):
+    return train_generator(data_dir)
+
+
 def build_model():
-    backbone = MobileNetV2((224, 224, 3), alpha=0.5, include_top=False)
+    backbone = MobileNetV2((IMAGE_SIZE, IMAGE_SIZE, 3), alpha=0.35, include_top=False)
 
     cur = backbone.output
     cur = GlobalAveragePooling2D()(cur)
-    cur = Dense(1, activation='sigmoid')(cur)
+    if N_CLASSES == 1 or N_CLASSES == 2:
+        act_f = 'sigmoid'
+        loss_f = 'binary_crossentropy'
+    else:
+        assert N_CLASSES > 1
+        act_f = 'softmax'
+        loss_f = 'sparse_categorical_crossentropy'
+    #cur = Dropout(0.2)(cur)
+    cur = Dense(N_CLASSES, activation=act_f)(cur)
 
     model = Model(backbone.input, cur)
-    model.compile(optimizer=Adam(lr=3e-4), loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(lr=LR), loss=loss_f, metrics=['accuracy'])
+    return model
+
+
+def train_model(train_data_path, test_data_path, model):
     model.summary()
-
-    return model
-
-
-def train_model(train_data_path):
     train_data_generator = train_generator(train_data_path)
+    test_data_generator = test_generator(test_data_path)
 
-    model = load_model('model.hdf5')
-    #model = build_model()
-    model.fit_generator(train_data_generator, steps_per_epoch=IMAGE_COUNT, epochs=1)
+    model.fit_generator(train_data_generator, steps_per_epoch=train_data_generator.samples // BATCH_SIZE, epochs=EPOCHS,
+                        validation_data=test_data_generator, validation_steps=test_data_generator.samples // BATCH_SIZE)
     model.save(MODEL_NAME)
-    return model
 
 
 if __name__ == '__main__':
-    train_model(DATASET_PATH)
+    if args.continue_from_model is None:
+        model = build_model()
+    else:
+        model = load_model(args.continue_from_model)
+
+    train_model(TRAIN_DATASET_PATH, TEST_DATASET_PATH, model)
